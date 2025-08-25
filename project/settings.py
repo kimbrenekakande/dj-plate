b2ad035b9dfa7b0606233935f64c0cfa8a4fb67f
Kimbrene Kakande <kimbrenekakande@gmail.com>
2025-08-25 08:50:53 +0300

    dashboard init

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4so3@d!&*xy=#lt12zw4869#sayu@u@$6-8ul-48-1y+p2^bp(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
