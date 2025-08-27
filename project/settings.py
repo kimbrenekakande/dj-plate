e0981b3bda3613d45f7145c7c09d8af1ed51041d
kimbrene <kakande@kimbrenes-iMac.local>
2025-08-27 18:08:30 +0300

    shifting the project to both uv and django-hmx instead of htmx cdn

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     #Third Party Apps
-    "tailwind","theme","django_cotton",
+    "tailwind","theme","django_cotton","django_htmx",
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 if DEBUG:
